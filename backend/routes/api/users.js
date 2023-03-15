ab34a808bcb287a56ced9cfc1cfefae391d8c1eb
root <root@Im-Laptop.localdomain>
2023-03-15 17:57:05 -0400

    auth changes1

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -34,7 +34,7 @@ router.post(
     validateSignup,
     async (req, res) => {
       const { firstName, lastName, email, password, username} = req.body;
-      const user = await User.signup({ firstName, lastName, email, username, password});
+      const user = await User.signup({ firstName, lastName, email, password, username});
 
       await setTokenCookie(res, user);
 
