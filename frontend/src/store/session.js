a82b63910c57d0f3f1da799126669b1d5733272a
root <root@Im-Laptop.localdomain>
2023-04-29 17:24:13 -0400

    signupform

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -43,6 +43,30 @@ export const login = (user) => async dispatch => {
     }
 }
 
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
 //state
 
 const initialState = { user: null };
