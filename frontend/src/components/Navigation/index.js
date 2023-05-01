c95f384e54b6594bfaec59693a3f71e774b039e4
root <root@Im-Laptop.localdomain>
2023-05-01 08:44:37 -0400

    profileButton

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import ProfileButton from './ProfileButton';
+import ProfileButton from './ProfileButton'
 import * as sessionActions from '../../store/session';
-// import './Navigation.css';
+import './Navigation.css';
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
