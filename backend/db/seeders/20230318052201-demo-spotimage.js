ac9126c2fe23d9b87c857b910b572443df5b0e8a
imran shaukat <imransh11@gmail.com>
2024-05-06 10:30:08 -0400

    seeds added

--- backend/db/seeders/20230318052201-demo-spotimage.js
+++ backend/db/seeders/20230318052201-demo-spotimage.js
@@ -26,6 +26,16 @@ module.exports = {
         preview: false
       },
       {
+        spotId: 1,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-1100679294345775218/original/4ddc7a15-727d-462d-8638-091ac923d4fc.jpeg',
+        preview: false
+      },
+      {
+        spotId: 1,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-1100679294345775218/original/10b8c389-f9a1-4ddb-8165-7d430cf6bd12.jpeg?im_w=1200',
+        preview: false
+      },
+      {
         spotId: 2,
         url: 'https://a0.muscache.com/im/pictures/da2eff5d-90b5-442b-9ca0-70dd6c4928cf.jpg?im_w=1200',
         preview: true
@@ -44,6 +54,86 @@ module.exports = {
         spotId: 2,
         url: 'https://a0.muscache.com/im/pictures/7cd01ad9-c70e-4f54-9cff-12f684e2fadd.jpg?im_w=1200',
         preview: false
+      },
+      {
+        spotId: 2,
+        url: 'https://a0.muscache.com/im/pictures/7cd01ad9-c70e-4f54-9cff-12f684e2fadd.jpg?im_w=1200',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://a0.muscache.com/im/pictures/65bfe904-c752-4d71-b4f8-a2d8de1ecc5f.jpg?im_w=720',
+        preview: true
+      },
+      {
+        spotId: 3,
+        url: 'https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6NDQxNzg5MTY%3D/original/d6c5466e-d231-40c9-bd14-ab2ee5377be5.jpeg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://a0.muscache.com/im/pictures/cde07077-c8b7-4434-a739-165c9ca3786e.jpg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6NDQxNzg5MTY%3D/original/fe0998f5-ecdc-4c7a-9e17-7c4e6aa9ce05.jpeg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-44178916/original/bc1a880d-9d1e-4da6-9df9-08c2aa9f0bfa.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/d0d292eb-0243-4700-ad1e-0edbb975d65e.jpg?im_w=960',
+        preview: true
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/9238119a-4f74-4dc1-b9b2-230cc21ba1c5.jpg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/a8ee4602-1c9a-49db-9756-8dcbf07a6d6f.jpg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/d8af04cc-2c85-46ca-8e1c-b0ebd648cb62.jpg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/70ddb8ee-1e2f-49e6-bec4-beb7de047a86.jpg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-23853582/original/b7bfead3-9a01-4807-9a14-e7ac93bea04e.jpeg?im_w=960',
+        preview: true
+      },
+      {
+        spotId: 5,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-23853582/original/c3eb035a-c0b5-4df6-96bf-39ad849dd44b.jpeg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-23853582/original/f459bd79-8425-4cc4-8dc3-7c30107619cb.jpeg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-23853582/original/ff26785b-e496-4363-91ec-2605b1603105.jpeg?im_w=720',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-23853582/original/d38fef1e-7e41-4cca-b952-2b5182071295.jpeg?im_w=720',
+        preview: false
       }
     ], {})
   },
@@ -52,7 +142,7 @@ module.exports = {
     options.tableName = 'SpotImages';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: [1, 2]}
+      spotId: { [Op.in]: [1, 2, 3, 4, 5]}
     }, {})
   }
 };
--- backend/db/seeders/20230318171238-demo-review.js
+++ backend/db/seeders/20230318171238-demo-review.js
@@ -27,6 +27,24 @@ module.exports = {
         spotId: 2,
         review: "This was an awesome spot!",
         stars: 5,
+      },
+      {
+        userId: 3,
+        spotId: 5,
+        review: "This was an awesome spot!",
+        stars: 4,
+      },
+      {
+        userId: 4,
+        spotId: 3,
+        review: "This was an awesome spot!",
+        stars: 5,
+      },
+      {
+        userId: 5,
+        spotId: 3,
+        review: "This was an awesome spot!",
+        stars: 4,
       }
     ], {})
   },
@@ -35,7 +53,7 @@ module.exports = {
     options.tableName = 'Reviews';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: [1, 2]}
+      spotId: { [Op.in]: [1, 2, 3, 4, 5]}
     }, {})
   }
 };
