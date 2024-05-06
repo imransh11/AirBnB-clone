2045c9636b31769941500758560e67f611f29b35
imran shaukat <imransh11@gmail.com>
2024-05-06 13:54:41 -0400

    red color added to login error/console log removed from spotDetail

--- frontend/src/components/Spots/spotdetail.js
+++ frontend/src/components/Spots/spotdetail.js
@@ -16,7 +16,7 @@ const SpotDetails = () => {
     const spotClicked = useSelector((state) => state.spots[spotId])
     const sessionUser = useSelector(state => state.session)
     const spotReviews = useSelector(state => Object.values(state.reviews))
-    console.log(spotClicked,spotReviews, spotId, sessionUser,'spot----byID-----------')
+    // console.log(spotClicked,spotReviews, spotId, sessionUser,'spot----byID-----------')
 
 
     useEffect(() => {
